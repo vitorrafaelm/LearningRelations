42b901c1184af442d57cce828a1f206406eb0db2
vitorrafaelm <vitor.rafael1518@gmail.com>
2021-05-31 17:40:33 -0300

    Error no uso do swagger-autogen

--- backend-/src/server.ts
+++ backend-/src/server.ts
@@ -1,12 +1,15 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes/index';
+import swaggerUI from 'swagger-ui-express';
 
 import './database/index';
+import swaggerFile from './swagger/swagger_output.json';
 
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use(routes);
 
 app.listen(3333, () => {
